8188ce3f650f7d857acba7e4c8b8d66de0c985e4
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-17 11:55:40 +0800

    Create ex6.cpp

---
+++ ch4/ex6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+struct CandyBar{
+	string name;
+	double weight;
+	int calories;
+};
+
+int main() {
+	CandyBar snack[3] = {
+		{"M&M",10,4},
+		{"Snacky snack",5,8},
+		{"Gummy Bear",6,4}
+	} ;
+	for(int i=0;i<3;i++){
+	cout<<"Name of a candy bar is "<<snack[i].name<<"."<<endl;
+	cout<<"Weight is of the candy bar is "<<snack[i].weight<<"."<<endl;
+	cout<<"Calories of the candy bar is "<<snack[i].calories<<"."<<endl;
+	}
+	return 0;
+}
