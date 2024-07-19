efb2a07a538721399900cb22552582f2d76c043d
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-19 09:06:43 +0800

    ex1.cpp

--- ch5/ex1.cpp
+++ ch5/ex1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main (){
+	int n1,n2,sum=0;
+	cout<<"Enter the first number: ";
+	cin>>n1;
+	cout<<"Enter the second number: ";
+	cin>>n2;
+	for (int i=n1;i<=n2;i++)
+		sum=sum + i;
+	cout<<"Sum of all intergers = "<<sum;
+	return 0;
+}
