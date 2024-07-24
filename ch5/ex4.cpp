09b6227b484a54a0504e8329d3a25dd4d51dc7ed
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-24 13:53:23 +0800

    ex4.cpp

--- ch5/ex4.cpp
+++ ch5/ex4.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 int main() {
     double Daphne=110,Cleo=105;
-    for(int year = 1;Cleo<Daphne;year++){
+    int year = 1;
+    for(;Cleo<Daphne;year++){
         Daphne += 100*0.10;
         Cleo += Cleo*0.05;
     }
+    cout<<"Years taken for the value of Cleo investment to exceed"<<endl;
+    cout<<"the value of Daphne investment: "<<year<<endl;
+    cout<<"Value of Daphne investment: $"<<Daphne<<endl;
+    cout<<"Value of Cleo investment: $"<<Cleo;
     return 0;
 }
