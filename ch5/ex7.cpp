761e54ae3f6fef6047cc42f31ef40b241c8ebb62
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-24 15:48:42 +0800

    ex7.cpp

--- ch5/ex7.cpp
+++ ch5/ex7.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+struct car {
+	string make;
+	int year;
+};
+
+int main (){
+	int number;
+	cout<<"How many cars do you wish to catalog? ";
+	cin>>number;
+		car * cars = new car [number];
+	
+	for(int i=0; i<number;i++){
+		cout<<"Car #"<<i+1<<endl;
+		cout<<"Please enter the make: ";
+		cin.get();
+		getline(cin,cars[i].make);
+		cout<<"Please enter the year made: ";
+		cin>>cars[i].year;
+	}
+	cout<<"Here is your collection: "<<endl;
+	for(int t=0;t<number;t++){	
+	cout<<cars[t].year<<" "<<cars[t].make<<endl;
+    }
+    return 0;
+}
