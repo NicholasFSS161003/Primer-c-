de143ad7af30a44ba9f065442bb1b393a0eb2386
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-25 11:35:21 +0800

    ex8.cpp

--- ch5/ex8.cpp
+++ ch5/ex8.cpp
@@ -1,1 +1,15 @@
+#include<cstring>
+#include <iostream>
+using namespace std;
 
+int main() {
+    char word[100];
+    int count=0;
+    cout<<"Enter words (to stop, type the word done):"<<endl;
+    for(int i=0;strcmp(word,"done");i++){
+        cin >> word;
+        count++;
+    }
+    cout<<"You entered a total of "<<count-1<<" words";
+    return 0;
+}
