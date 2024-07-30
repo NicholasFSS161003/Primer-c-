5bfd4d72396617f7542c7113a39809380c944565
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-30 10:45:33 +0800

    ex1.cpp

--- ch6/ex1.cpp
+++ ch6/ex1.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <cctype>
+#include <cstring>
+
 int main(){
 using namespace std;
     char input[50];
     cout<<"Enter something: ";
     int i=0;
     while (cin.get(input,50) && int(input[i]) != 64){
-        if(isupper(input[i]))
-            input[i]=tolower(input[i]);
+        for(int e=0;e<strlen(input);e++){
+            if(isupper(input[e]))
+             input[e]=tolower(input[e]);
+        }
         cout<<input;
         i++;
     }
