7cf5d56b43dd389e0acc13106a62ac3761e1f51d
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-07-30 10:39:22 +0800

    ex1.cpp

--- ch6/ex1.cpp
+++ ch6/ex1.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+#include <cctype>
+int main(){
+using namespace std;
+    char input[50];
+    cout<<"Enter something: ";
+    int i=0;
+    while (cin.get(input,50) && int(input[i]) != 64){
+        if(isupper(input[i]))
+            input[i]=tolower(input[i]);
+        cout<<input;
+        i++;
+    }
+    cout<<endl;
+    cout<<"Program ended!";
+return 0;
+}
