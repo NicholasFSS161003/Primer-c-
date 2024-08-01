70af018e01e05adfb11f8600ddb334b24adbe161
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-01 12:01:20 +0800

    Update ex1.cpp

--- ch7/ex1.cpp
+++ ch7/ex1.cpp
@@ -1,1 +1,22 @@
+#include <Servo.h>
+Servo Servo;
+
+void setup() {
+servo.attach(A0);
+pinMode(2,INPUT);
+pinMode(8,INPUT);
+pinMode(A0,INPUT);
+digitalWrite(2,HIGH);
+digitalWrite(8,HIGH);
+analogWrite(A0,HIGH);
+}
+
+void loop() {
+if(digitalRead(8)==0){
+Servo.write(180);
+}
+if(digitalRead(2)==1){
+Servo.write(0);
+}
+}
 
